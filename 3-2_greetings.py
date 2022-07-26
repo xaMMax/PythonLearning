b306c2047e0e68ddee0cfdb4f347628e899bc277
xaMMax <petrolijuice@gmail.com>
2022-07-26 20:09:54 +0200

    correct tasks and add new

---
+++ 3-2_greetings.py
@@ -0,0 +1,3 @@
+friends_names = ['Anna', 'Dariia', 'Vlad', 'Natali', 'Serega', 'Vetal', 'Evgen']
+for name in friends_names:
+    print(f'Hello, nice to meet you {name}')
---
+++ 3-3_personal_list.py
@@ -0,0 +1,3 @@
+car_models = ['bmw e39', 'bmw e38', 'bmw e60', 'bmw g05', 'bmw e53', 'bmw e34']
+for element in car_models:
+    print(f'I would like to own a {element} at that moment')
