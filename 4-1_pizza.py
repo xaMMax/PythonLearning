7b493dc749fbb70f64d6c75990fbbb1df995b4a7
xaMMax <petrolijuice@gmail.com>
2022-08-03 12:20:36 +0200

    add new tasks

---
+++ 4-1_pizza.py
@@ -0,0 +1,4 @@
+list_of_pizza = ['mozzarella', '4 cheeses', 'pepperoni']
+for pizza in list_of_pizza:
+    print('I like', pizza, 'pizza')
+print('I really love pizza')
---
+++ 4-2_animals.py
@@ -0,0 +1,4 @@
+list_of_animals = ['Cat', 'Tiger', 'Lion']
+for animal in list_of_animals:
+    print(f'A {animal} would make a great pet')
+print('Any of these animals have a nice wool')
